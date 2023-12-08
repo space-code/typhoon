0c627f11f8b43b4dbc36c00cd3cf26e6c5d09839
Nikita Vasilev <nv3212@gmail.com>
2023-12-08 14:27:23 +0100

    Implement exponential backoff with jitter

--- Sources/Typhoon/Classes/Strategy/RetryPolicyStrategy.swift
+++ Sources/Typhoon/Classes/Strategy/RetryPolicyStrategy.swift
@@ -8,10 +8,35 @@ import Foundation
 /// A strategy used to define different retry policies.
 public enum RetryPolicyStrategy {
     /// A retry strategy with a constant number of attempts and fixed duration between retries.
+    ///
+    /// - Parameters:
+    ///   - retry: The number of retry attempts.
+    ///   - duration: The initial duration between retries.
     case constant(retry: Int, duration: DispatchTimeInterval)
 
     /// A retry strategy with an exponential increase in duration between retries.
-    case exponential(retry: Int, multiplier: Double, duration: DispatchTimeInterval)
+    ///
+    /// - Parameters:
+    ///   - retry: The number of retry attempts.
+    ///   - multiplier: The multiplier for calculating the exponential backoff duration (default is 2).
+    ///   - duration: The initial duration between retries.
+    case exponential(retry: Int, multiplier: Double = 2, duration: DispatchTimeInterval)
+
+    /// A retry strategy with exponential increase in duration between retries and added jitter.
+    ///
+    /// - Parameters:
+    ///   - retry: The number of retry attempts.
+    ///   - jitterFactor: The factor to control the amount of jitter (default is 0.1).
+    ///   - maxInterval: The maximum allowed interval between retries (default is 60 seconds).
+    ///   - multiplier: The multiplier for calculating the exponential backoff duration (default is 2).
+    ///   - duration: The initial duration between retries.
+    case exponentialWithJitter(
+        retry: Int,
+        jitterFactor: Double = 0.1,
+        maxInterval: UInt64? = 60,
+        multiplier: Double = 2,
+        duration: DispatchTimeInterval
+    )
 
     /// The number of retry attempts based on the strategy.
     public var retries: Int {
@@ -19,6 +44,8 @@ public enum RetryPolicyStrategy {
         case let .constant(retry, _):
             return retry
         case let .exponential(retry, _, _):
+            return retry
+        case let .exponentialWithJitter(retry, _, _, _, _):
             return retry
         }
     }
@@ -29,6 +56,8 @@ public enum RetryPolicyStrategy {
         case let .constant(_, duration):
             return duration
         case let .exponential(_, _, duration):
+            return duration
+        case let .exponentialWithJitter(_, _, _, _, duration):
             return duration
         }
     }

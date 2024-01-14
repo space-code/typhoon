3152f1e2552b1080ecb2959f605abc14e1b3bfda
Nikita Vasilev <nv3212@gmail.com>
2024-01-14 19:21:48 +0100

    Add support for Swift 5.8

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Typhoon",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(name: "Typhoon", targets: ["Typhoon"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Typhoon", dependencies: []),
+        .testTarget(name: "TyphoonTests", dependencies: ["Typhoon"]),
+    ]
+)

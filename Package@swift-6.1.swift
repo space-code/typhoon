fd0eed04efb9a77ae300efc4867820a624c605d9
Nikita Vasilev <nv3212@gmail.com>
2025-11-16 19:30:16 +0400

    Update `Package.swift`

--- Package@swift-6.1.swift
+++ Package@swift-6.1.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,7 @@ let package = Package(
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6),
+        .visionOS(.v1),
     ],
     products: [
         .library(name: "Typhoon", targets: ["Typhoon"]),
